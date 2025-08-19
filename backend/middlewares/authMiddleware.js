c33eabb0c3e344a68b903364ae33215f8f1f51b4
Pedro Vasconcellos <pedro.vasconcellos@zuvia.com.br>
2025-08-19 18:01:15 -0300

    19-08-25

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({ message: "Acesso negado: Token não fornecido"})
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = decoded;
+        next();
+    }catch(err){
+        res.status(400).json({ message: "Acesso negado: token inválido "});
+    }
+};
+
+export default verifyToken;
